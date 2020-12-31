099fb83a84e5b5f4122390122f5fae569f7f85b6
brooks-brothers-007 <shoutanakane@gmail.com>
2020-12-31 19:39:01 +0900

    Deviseを導入し、ユーザー用のモデルおよびテーブルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # devise_for :usersを削除する事でdb:migrate、rails g deviseを行えた。
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
 end
