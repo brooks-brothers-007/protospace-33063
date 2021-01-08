55be829bdc2cfa5c2468020d3719bca3fc1eafb9
brooks-brothers-007 <shoutanakane@gmail.com>
2021-01-08 12:42:27 +0900

    protospaceの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, :profile, :occupation, :position,  presence: true       
- 
-         #  validates :name, presence: true
-        #  validates :profile, presence: true
-        #  validates :occupation, presence: true 
-        #  validates :position, presence: true
+         validates :name, :profile, :occupation, :position,presence: true
+     
+         has_many :comments
+         has_many :prototypes
 end
